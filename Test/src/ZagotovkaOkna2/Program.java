337b736e29764bcc518211b9df471df7c5c30a5a
NorfiS <v.l.64@yandex.ru>
2016-05-19 17:44:44 +0300

    New Zagotovka

---
+++ Test/src/ZagotovkaOkna2/Program.java
@@ -0,0 +1,10 @@
+package ZagotovkaOkna2;
+
+import javax.swing.JFrame;
+
+public class Program {
+	public static void main (String [] args) {
+		JFrame myWindow = new Window();
+		myWindow.setVisible(true);
+		}
+}
---
+++ Test/src/ZagotovkaOkna2/Window.java
@@ -0,0 +1,13 @@
+package ZagotovkaOkna2;
+
+import javax.swing.JFrame;
+
+public class Window extends JFrame{
+	Window(){
+		setTitle("Frame");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(250, 100);
+		setLocationRelativeTo(null);
+		setResizable(false);
+		}
+}
